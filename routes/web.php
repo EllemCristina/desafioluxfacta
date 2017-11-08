a47b5c173365533cb03aa6595813184b0b1595eb
EllemCristina <ellem.ferraciolli@gmail.com>
2017-11-08 20:12:49 -0200

    comentários inseridos

--- routes/web.php
+++ routes/web.php
@@ -16,16 +16,16 @@ Route::get('/', function () {
 });
 
 
-Route::get('/enquetes', 'EnqueteController@lista');
+Route::get('/enquetes', 'EnqueteController@lista'); //traz todas enquetes
 
-Route::post('/enquete/{desc}/{op1}/{op2}/{op3}', 'EnqueteController@novo');
+Route::post('/enquete/{desc}/{op1}/{op2}/{op3}', 'EnqueteController@novo'); //adiciona uma nova enquete
 
-Route::get('/enquete/{id}', 'EnqueteController@consulta');
+Route::get('/enquete/{id}', 'EnqueteController@consulta'); //consulta uma enquete por id
 
-Route::get('/enquete/{id}/stats', 'EnqueteController@estatisticas');
+Route::get('/enquete/{id}/stats', 'EnqueteController@estatisticas'); //traz a quantidade de votos da enquete
 
-Route::put('/enquete/{desc}/{id}', 'EnqueteController@editar');
+Route::put('/enquete/{desc}/{id}', 'EnqueteController@editar');  //edita a descrição da enquete
 
-Route::post('/enquete/vote/{op}/{id}', 'EnqueteController@votar');
+Route::post('/enquete/vote/{op}/{id}', 'EnqueteController@votar'); //vota em uma opção específica e id
 
-Route::delete('/enquete/{id}', 'EnqueteController@excluir');
+Route::delete('/enquete/{id}', 'EnqueteController@excluir'); //exclui uma enquete
